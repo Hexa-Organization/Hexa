2b8bb9cbe8295d3bd199ec4c6ed23316d362d884
bohou <bohou@DESKTOP-10AQPNL>
2019-06-13 21:58:19 +0200

    Projekt je úspěšně apDejtován ale spustit nejede někde je chyba a protože už to dělám po druhé a je mi zima tak si beru volono

--- src/doupedraku/MapController.java
+++ src/doupedraku/MapController.java
@@ -3,12 +3,16 @@ package doupedraku;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 public class MapController implements Initializable 
 {
@@ -43,9 +47,32 @@ public class MapController implements Initializable
     int tree4x = 8;
     int tree4y = 4;
     
+    //enemy
+    
+    @FXML
+    public ImageView enemy1;
+    int enemy1x = 2;
+    int enemy1y = 5;
+    @FXML
+    public ImageView enemy2;
+    int enemy2x = 9;
+    int enemy2y = 7;
+    @FXML
+    public ImageView enemy3;
+    int enemy3x = 1;
+    int enemy3y = 3;
+    @FXML
+    public ImageView enemy4;
+    int enemy4x = 5;
+    int enemy4y = 4;
+    
     int fieldx[] = new int[5];
     int fieldy[] = new int[5];
     ImageView trees[] = new ImageView[5];
+    
+    int field2x[] = new int[5];
+    int field2y[] = new int[5];
+    ImageView enemys[] = new ImageView[5];
     
     private void Generate()
     {
@@ -54,6 +81,7 @@ public class MapController implements Initializable
         trees[2] = tree2;
         trees[3] = tree3;
         trees[4] = tree4;
+        
         fieldx[0] = tree0x;
         fieldy[0] = tree0y;
         fieldx[1] = tree1x;
@@ -63,11 +91,30 @@ public class MapController implements Initializable
         fieldx[3] = tree3x;
         fieldy[3] = tree3y;
         fieldx[4] = tree4x;
-        fieldy[4] = tree4y; 
+        fieldy[4] = tree4y;
+        
+        enemys[1] = enemy1;
+        enemys[2] = enemy2;
+        enemys[3] = enemy3;
+        enemys[4] = enemy4;
+        
+        field2x[1] = enemy1x;
+        field2y[1] = enemy1y;
+        field2x[2] = enemy2x;
+        field2y[2] = enemy2y;
+        field2x[3] = enemy3x;
+        field2y[3] = enemy3y;
+        field2x[4] = enemy4x;
+        field2y[4] = enemy4y;
         
         for (int i = 0; i < trees.length; i++)
         {
             grid.setConstraints(trees[i], fieldx[i], fieldy[i]);
+        }
+        
+        for (int i = 0; i < enemys.length; i++)
+        {
+            grid.setConstraints(enemys[i], field2x[i], field2y[i]);
         }
     }
     
@@ -75,6 +122,24 @@ public class MapController implements Initializable
     private void handleButtonAction(KeyEvent event) 
     {
         Movement(event);
+    }
+    
+     @FXML
+    public void Fighting()
+    {
+        try
+        {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Battle.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();        
+        }
+        
+        catch(Exception e)
+        {
+            System.out.println("Nefunguje to!!");
+        }
     }
     
     @FXML
@@ -93,7 +158,13 @@ public class MapController implements Initializable
                 {
                     System.out.println("NO");
                     grid.setConstraints(chr, --x, y);
-                }  
+                }
+                
+                else if(EnemyPosition())
+                {
+                    System.out.println("Fight");
+                    Fighting();
+                }
                 break;
 
             case A:
@@ -107,6 +178,13 @@ public class MapController implements Initializable
                     System.out.println("NO");
                     grid.setConstraints(chr, ++x, y);
                 }
+                
+                 else if(EnemyPosition())
+                {
+                    System.out.println("Fight");
+                    Fighting();
+                }
+                
                 break;
 
             case S:
@@ -120,6 +198,13 @@ public class MapController implements Initializable
                     System.out.println("NO");
                     grid.setConstraints(chr, x, --y);
                 }
+                
+                 else if(EnemyPosition())
+                {
+                    System.out.println("Fight");
+                    Fighting();
+                }
+              
                 break;
 
             case W:
@@ -133,6 +218,13 @@ public class MapController implements Initializable
                     System.out.println("NO");
                     grid.setConstraints(chr, x, ++y);
                 }
+                
+                   else if(EnemyPosition())
+                {
+                    System.out.println("Fight");
+                    Fighting();
+                }
+              
                 break;
         }       
     }
@@ -142,6 +234,18 @@ public class MapController implements Initializable
         for (int i = 0; i < fieldx.length; i++)
         {
             if (x == fieldx[i] && y == fieldy[i])
+            {
+                return true;
+            }     
+        }
+        return false;
+    }
+    
+    private boolean EnemyPosition()
+    {
+        for (int i = 0; i < field2x.length; i++)
+        {
+            if (x == field2x[i] && y == field2y[i])
             {
                 return true;
             }     
